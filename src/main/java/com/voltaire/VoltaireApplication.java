2c6f3942b6fc866fe45f1197860a9f040e135477
Vukasin Micic <vukasin.micic5@gmail.com>
2020-09-29 10:28:18 +0200

    Init project

---
+++ src/main/java/com/voltaire/VoltaireApplication.java
@@ -0,0 +1,13 @@
+package com.voltaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoltaireApplication.class, args);
+	}
+
+}
