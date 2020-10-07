fbaa8af114bb12167b46817d44fee82079146bb3
Vukasin Micic <vukasin.micic5@gmail.com>
2020-10-07 10:15:33 +0200

    Update IdResponse with annotations

--- src/main/java/com/voltaire/shared/IdResponse.java
+++ src/main/java/com/voltaire/shared/IdResponse.java
@@ -1,19 +1,11 @@
 package com.voltaire.shared;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class IdResponse {
 
     private Long id;
-
-    public IdResponse(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
