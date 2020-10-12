c5914e665780b996aedc65ccb51ce9c79c812956
Vukasin Micic <vukasin.micic5@gmail.com>
2020-10-12 09:19:09 +0200

    Add restaurant service integration test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,6 @@ INSERT INTO restaurant(id, name, address, opening_time, closing_time) VALUES
 
 INSERT INTO menu_item(id, name, description, price, restaurant_id) VALUES
     ('500', 'Burger', 'junece meso, lepinja, kecap', '250.0', '1000'),
-    ('501', 'Burito', 'junece meso, tortilja, pasulj', '300 .0', '1000'),
+    ('501', 'Burito', 'junece meso, tortilja, pasulj', '300.0', '1000'),
     ('502', 'Sendvic piletina', 'piletina, lepinja, salata', '250.0', '1001');
 
--- src/test/java/com/voltaire/restaurant/RestaurantServiceUnitTest.java
+++ src/test/java/com/voltaire/restaurant/RestaurantServiceUnitTest.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class RestaurantServiceTest {
+class RestaurantServiceUnitTest {
 
     @Mock
     RestaurantRepository restaurantRepository;
