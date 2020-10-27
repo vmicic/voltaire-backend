d4482619ad1a7a109726ef1619e11db5ace1b640
Vukasin Micic <vukasin.micic5@gmail.com>
2020-10-27 10:33:52 +0100

    Add log output for api gateway header

--- src/main/java/com/voltaire/security/ApiKeyAuthFilter.java
+++ src/main/java/com/voltaire/security/ApiKeyAuthFilter.java
@@ -1,10 +1,14 @@
 package com.voltaire.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ApiKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final String apiKeyHeaderName;
 
@@ -14,6 +18,8 @@ public class ApiKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
 
     @Override
     protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        var headerValue = request.getHeader("X-Apigateway-Api-Userinfo");
+        log.info("This is header value: " + headerValue);
         return request.getHeader(apiKeyHeaderName);
     }
 
